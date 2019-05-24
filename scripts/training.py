e1a4f286c7350785fed79fe48813a5dac3a0d80a
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2019-05-24 18:03:09 -0500

    Run a bigger CNN over weekend

--- scripts/training.py
+++ scripts/training.py
@@ -5,6 +5,7 @@ import sys; sys.path.append("/home/lls/DeepHalos/")
 import data_processing as dp
 import CNN
 from tensorflow import set_random_seed
+from tensorflow.keras.utils import plot_model
 import pickle
 
 
@@ -15,14 +16,16 @@ if __name__ == "__main__":
     halo_mass = np.load("/home/lls/stored_files/halo_mass_particles.npy")
     scaler, normalised_mass = dp.normalise_output(halo_mass, take_log=True)
     p_ids = np.where(halo_mass > 0)[0]
-    p_ids_saved = np.load("/share/data2/lls/deep_halos/subboxes/ids_saved.npy")
 
     ######### DATA PROCESSING ###########
 
-    training_ids = np.random.choice(p_ids_saved, 50000, replace=False)
-    np.save("/share/data2/lls/deep_halos/training_ids.npy", training_ids)
-    validation_ids = np.random.choice(p_ids_saved, 10000, replace=False)
-    np.save("/share/data2/lls/deep_halos/validation_ids.npy", validation_ids)
+    # p_ids_saved = np.load("/share/data2/lls/deep_halos/subboxes/ids_saved.npy")
+    # training_ids = np.random.choice(p_ids_saved, 50000, replace=False)
+    # np.save("/share/data2/lls/deep_halos/training_ids.npy", training_ids)
+    # validation_ids = np.random.choice(p_ids_saved, 10000, replace=False)
+    # np.save("/share/data2/lls/deep_halos/validation_ids.npy", validation_ids)
+    training_ids = np.load("/share/data2/lls/deep_halos/training_ids.npy")
+    validation_ids = np.load("/share/data2/lls/deep_halos/validation_ids.npy")
 
     # for Data generators need ids to be strings and
 
@@ -37,11 +40,11 @@ if __name__ == "__main__":
 
     set_random_seed(7)
     param_conv = {'conv_1': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'strides': 2, 'padding':'same',
-                             'pool': True, 'bn': True},
-                  'conv_2': {'num_kernels': 4, 'dim_kernel': (3, 3, 3), 'strides': 2, 'padding':'same',
-                             'pool': False, 'bn': True},
-                  # 'conv_3': {'num_kernels': 32, 'dim_kernel': (2, 2, 2), 'strides': 1, 'padding':'valid',
-                  #            'pool': False, 'bn': True},
+                             'pool': True, 'bn': True}, # output is 13x3x13
+                  'conv_2': {'num_kernels': 4, 'dim_kernel': (4, 4, 4), 'strides': 1, 'padding':'same',
+                             'pool': True, 'bn': True}, # output is 5x5x5
+                  'conv_3': {'num_kernels': 10, 'dim_kernel': (3, 3, 3), 'strides': 1, 'padding':'valid',
+                             'pool': False, 'bn': True}, # output is 3x3x3
                   # 'conv_4': {'num_kernels': 64, 'dim_kernel': (2, 2, 2), 'strides': 2, 'padding':'valid',
                   #            'pool': False, 'bn': True},
                   # 'conv_5': {'num_kernels': 100, 'dim_kernel': (3, 3, 3), 'strides': 1, 'padding':'valid',
@@ -53,26 +56,29 @@ if __name__ == "__main__":
     param_fcc = {'dense_1': {'neurons': 256, 'dropout': 0.5},
                  'dense_2': {'neurons': 128, 'dropout': 0.5}}
 
-    Model = CNN.CNN(training_generator, validation_generator, param_conv, param_fcc, num_epochs=10,
-                    use_multiprocessing=True, workers=24)
+    Model = CNN.CNN(training_generator, validation_generator, param_conv, param_fcc, num_epochs=50,
+                    use_multiprocessing=True, workers=24, verbose=2)
     model = Model.model
     history = Model.history
 
     ########## PREDICT AND SAVE ############
+    saving_path = "/share/data2/lls/deep_halos/run_6/"
 
     pred_cnn_training = model.predict_generator(training_generator)
     training_log_mass = scaler.inverse_transform(pred_cnn_training).flatten()
-    np.save("/share/data2/lls/deep_halos/predicted_log_mass_training.npy", training_log_mass)
-    np.save("/share/data2/lls/deep_halos/true_log_mass_training.npy", np.log10(halo_mass[training_ids]))
+    np.save(saving_path + "predicted_log_mass_training.npy", training_log_mass)
+    np.save(saving_path + "true_log_mass_training.npy", np.log10(halo_mass[training_ids]))
 
     pred_cnn_val = model.predict_generator(validation_generator)
     val_log_mass = scaler.inverse_transform(pred_cnn_val).flatten()
-    np.save("/share/data2/lls/deep_halos/predicted_log_mass_validation.npy", val_log_mass)
-    np.save("/share/data2/lls/deep_halos/true_log_mass_validation.npy", np.log10(halo_mass[validation_ids]))
+    np.save(saving_path + "predicted_log_mass_validation.npy", val_log_mass)
+    np.save(saving_path + "true_log_mass_validation.npy", np.log10(halo_mass[validation_ids]))
 
-    f = open("/share/data2/lls/deep_halos/history_model.txt", "wb")
+    f = open(saving_path + "history_model.txt", "wb")
     f.write(str(Model.history.history))
     f.close()
+
+    plot_model(model, to_file=saving_path + 'model.png', show_shapes=True)
 
     #
     # cd /share/data2/lls/deep_halos/
