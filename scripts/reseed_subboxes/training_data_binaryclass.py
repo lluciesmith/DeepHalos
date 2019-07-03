162b6ad8db31dfac6d6a4bbf189febabb7046837
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2019-07-03 13:45:55 -0500

    d

--- scripts/reseed_subboxes/training_data_binaryclass.py
+++ scripts/reseed_subboxes/training_data_binaryclass.py
@@ -1,17 +1,24 @@
 import numpy as np
-import sys; sys.path.append("/home/lls/DeepHalos/")
-import data_processing as dp
 
 
-paths = ["/share/data1/lls/standard_reseed3/", "/share/data1/lls/standard_reseed4/", "/share/data1/lls/standard_reseed5"]
+def transform_halo_mass_to_binary_classification(halo_mass, threshold=1.8*10**12):
+    labels = np.zeros((len(halo_mass), ))
+    labels[halo_mass > threshold] = 1
+    return labels
 
-for path in paths:
-    halo_mass = np.load(path + "halo_mass_particles.npy")
-    particle_ids = np.array([line.rstrip("\n") for line in open(path + "subboxes_ids.txt")]).astype("int")
 
-    labels = dp.transform_halo_mass_to_binary_classification(halo_mass, threshold=2*10**12)
+if __name__ == "__main__":
+    paths = ["/share/data1/lls/standard_reseed3/", "/share/data1/lls/standard_reseed4/", "/share/data1/lls/standard_reseed5"]
 
-    t_0 = np.concatenate((particle_ids[np.random.choice(np.where(labels[particle_ids] == 0)[0], 10000, replace=False)],
-                          particle_ids[np.random.choice(np.where(labels[particle_ids] == 1)[0], 10000, replace=False)]))
-    # np.random.shuffle(t_0)
-    np.savetxt(path + "training_ids_binary_classification.txt", t_0, fmt="%i", delimiter=",")
+    for path in paths:
+        halo_mass = np.load(path + "halo_mass_particles.npy")
+        particle_ids = np.array([line.rstrip("\n") for line in open(path + "subboxes_ids.txt")]).astype("int")
+
+        labels = transform_halo_mass_to_binary_classification(halo_mass, threshold=2*10**12)
+
+        t_0 = np.concatenate((particle_ids[np.random.choice(np.where(labels[particle_ids] == 0)[0], 10000,
+                                                            replace=False)],
+                              particle_ids[np.random.choice(np.where(labels[particle_ids] == 1)[0], 10000,
+                                                            replace=False)]))
+        # np.random.shuffle(t_0)
+        np.savetxt(path + "training_ids_binary_classification.txt", t_0, fmt="%i", delimiter=",")
