1ad2eb2e71715ecdecd7b732ac1a4191f3abce18
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2019-07-30 12:28:04 -0500

    saved ids

--- transfer.py
+++ transfer.py
@@ -6,12 +6,21 @@ import numpy as np
 import re
 import os
 
-val_ids = []
-regex = re.compile(r'\d+')
-for filename in os.listdir("subboxes/")[:-1]:
-    val_ids.append(int(regex.findall(filename)[-1]))
+path = "/share/hypatia/lls/deep_halos/"
+dirs = [path + "training_sim/training_set/",
+        path + "reseed_1/training_set/",
+        path + "reseed_2/training_set/",
+        path + "reseed_3/training_set/",
+        path + "reseed_4/training_set/",
+        path + "reseed_5/training_set/"]
 
-val_ids = np.array(val_ids)
-print(len(val_ids))
-np.save("reseed5_subboxes_ids.npy", val_ids)
+for i in range(len(dirs)):
+    dir = dirs[i]
+    val_ids = []
+    regex = re.compile(r'\d+')
+    for filename in os.listdir(dir)[:-1]:
+        val_ids.append(int(regex.findall(filename)[-1]))
 
+    val_ids = np.array(val_ids)
+    print(len(val_ids))
+    np.save(dir + "../saved_ids_training_set.txt", val_ids)
