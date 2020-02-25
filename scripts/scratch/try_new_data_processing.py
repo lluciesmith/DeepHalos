ac9a660b4424ec936825555be5a922ebabaf18d1
Luisa Lucie-Smith <lls@chewbacca.star.ucl.ac.uk>
2020-02-25 18:40:33 +0000

    commit

--- scripts/scratch/try_new_data_processing.py
+++ scripts/scratch/try_new_data_processing.py
@@ -6,6 +6,7 @@ from tensorflow.keras.callbacks import CSVLogger
 from tensorflow.keras.callbacks import LearningRateScheduler
 import tensorflow
 import dlhalos_code.data_processing as tn
+import time
 
 
 ########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
@@ -20,11 +21,11 @@ training_sims = ["0", "2", "4", "5", "6"]
 validation_sims = ["1"]
 batch_size = 80
 rescale_mean = 1.005
-rescale_std = 0.0505
+rescale_std = 0.05050
 
 training_set = tn.InputsPreparation(training_sims, load_ids=True)
 generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
-                                      batch_size=batch_size, rescale_mean=0, rescale_std=1)
+                                      batch_size=batch_size, rescale_mean=rescale_mean, rescale_std=rescale_std)
 
 validation_set = tn.InputsPreparation(validation_sims, load_ids=True, scaler_output=training_set.labels_scaler,
                                       random_subset_each_sim=4000)
@@ -33,7 +34,7 @@ generator_validation = tn.DataGenerator(validation_set.particle_IDs, validation_
 
 ######### TRAINING MODEL ##############
 
-path_model = "/lfstev/deepskies/luisals/scratch/"
+path_model = "/lfstev/deepskies/luisals/scratch2/"
 
 # checkpoint
 filepath = path_model + "/model/weights.{epoch:02d}.hdf5"
@@ -42,12 +43,7 @@ checkpoint_call = callbacks.ModelCheckpoint(filepath, period=5)
 # save histories
 csv_logger = CSVLogger(path_model + "/training.log", separator=',', append=False)
 
-# decay the learning rate
-lr_decay = LearningRateScheduler(CNN.lr_scheduler)
-
-# callbacks_list = [checkpoint_call, csv_logger]
-callbacks_list = [checkpoint_call, csv_logger, lr_decay]
-
+callbacks_list = [checkpoint_call, csv_logger]
 tensorflow.compat.v1.set_random_seed(7)
 
 param_conv = {'conv_1': {'num_kernels': 4, 'dim_kernel': (3, 3, 3),
@@ -67,8 +63,8 @@ param_fcc = {  # 'dense_1': {'neurons': 1024, 'bn': False, 'dropout': 0.2},
 Model = CNN.CNN(param_conv, param_fcc, dim=(51, 51, 51),
                 training_generator=generator_training, validation_generator=generator_validation, validation_freq=1,
                 callbacks=callbacks_list, num_epochs=100,
-                use_multiprocessing=True, workers=8, max_queue_size=100,
-                verbose=1, model_type="regression", lr=0.0001, train=True)
+                use_multiprocessing=True, workers=2, max_queue_size=10,
+                verbose=1, model_type="regression", lr=0.001, train=True)
 
 
 
@@ -76,7 +72,7 @@ Model = CNN.CNN(param_conv, param_fcc, dim=(51, 51, 51),
 
 training_set = tn.InputsPreparation(training_sims, load_ids=True)
 generator_training2 = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
-                                      batch_size=100000, rescale_mean=0, rescale_std=1)
+                                      batch_size=100000, rescale_mean=rescale_mean, rescale_std=rescale_std)
 X_train, y_train = generator_training2[0]
 
 validation_set = tn.InputsPreparation(validation_sims, load_ids=True, scaler_output=training_set.labels_scaler,
@@ -92,5 +88,40 @@ Model2 = CNN.CNN(param_conv, param_fcc, dim=(51, 51, 51),
 
 history = Model2.model.fit(X_train, y_train, batch_size=80, verbose=1, epochs=100, validation_data=(X_val, y_val),
                           shuffle=True, callbacks=callbacks_list)
+
+
+
+####### tests
+
+s = tn.SimulationPreparation(["0", "1"])
+
+def run():
+    t0 = time.time()
+    training_sims = ["0", "1"]
+    training_set = tn.InputsPreparation(training_sims, load_ids=True)
+    generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
+                                         batch_size=10000, rescale_mean=0, rescale_std=1)
+    x, y = generator_training[0]
+    t1 = time.time()
+    print("Loading 10000 particles took " + str((t1 - t0) / 60))
+
+run()
+
+
+def run2():
+    all_sims = ["0", "1", "2", "4", "5", "6"]
+    s = tn.SimulationPreparation(all_sims)
+
+    t0 = time.time()
+    training_sims = ["0", "2", "4", "5", "6"]
+    training_set = tn.InputsPreparation(training_sims, load_ids=True)
+    generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
+                                          batch_size=100000, rescale_mean=0, rescale_std=1)
+    x, y = generator_training[0]
+    t1 = time.time()
+    print("Loading 100000 particles took " + str((t1 - t0) / 60))
+
+
+run2()
 
 
