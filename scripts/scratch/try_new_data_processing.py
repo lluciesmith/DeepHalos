b876f6716e43c591021b4b63a499275917382fa8
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-02-25 22:41:56 +0000

    loading from file

--- scripts/scratch/try_new_data_processing.py
+++ scripts/scratch/try_new_data_processing.py
@@ -7,6 +7,7 @@ from tensorflow.keras.callbacks import LearningRateScheduler
 import tensorflow
 import dlhalos_code.data_processing as tn
 import time
+import numpy as np
 
 
 ########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
@@ -14,10 +15,10 @@ import time
 
 # First you will have to load the simulation
 
-all_sims = ["0", "1", "2", "4", "5", "6"]
+all_sims = ["0", "1", "3", "4", "5"]
 s = tn.SimulationPreparation(all_sims)
 
-training_sims = ["0", "2", "4", "5", "6"]
+training_sims = ["0", "3", "4", "5"]
 validation_sims = ["1"]
 batch_size = 80
 rescale_mean = 1.005
@@ -25,12 +26,14 @@ rescale_std = 0.05050
 
 training_set = tn.InputsPreparation(training_sims, load_ids=True)
 generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
-                                      batch_size=batch_size, rescale_mean=rescale_mean, rescale_std=rescale_std)
+                                      batch_size=80000, rescale_mean=rescale_mean, rescale_std=rescale_std)
+X, y = generator_training[0]
 
 validation_set = tn.InputsPreparation(validation_sims, load_ids=True, scaler_output=training_set.labels_scaler,
                                       random_subset_each_sim=4000)
 generator_validation = tn.DataGenerator(validation_set.particle_IDs, validation_set.labels_particle_IDS, s.sims_dic,
-                                        batch_size=batch_size, rescale_mean=rescale_mean, rescale_std=rescale_std)
+                                        batch_size=4000, rescale_mean=rescale_mean, rescale_std=rescale_std)
+X_val1, y_val1 = generator_validation[0]
 
 ######### TRAINING MODEL ##############
 
@@ -64,64 +67,69 @@ Model = CNN.CNN(param_conv, param_fcc, dim=(51, 51, 51),
                 training_generator=generator_training, validation_generator=generator_validation, validation_freq=1,
                 callbacks=callbacks_list, num_epochs=100,
                 use_multiprocessing=True, workers=2, max_queue_size=10,
-                verbose=1, model_type="regression", lr=0.001, train=True)
+                verbose=1, model_type="regression", lr=0.001, train=False)
+
+history = Model.model.fit(X, y, batch_size=80, verbose=1, epochs=100, validation_data=(X_val1, y_val1),
+                          shuffle=True, callbacks=callbacks_list)
+np.save(path_model + "/history_100_epochs_mixed_sims.npy", history.history)
+Model.model.save(path_model + "/model_100_epochs_mixed_sims.h5")
 
 
 
 ########## OPTION 2 ######################
 
-training_set = tn.InputsPreparation(training_sims, load_ids=True)
-generator_training2 = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
-                                      batch_size=100000, rescale_mean=rescale_mean, rescale_std=rescale_std)
-X_train, y_train = generator_training2[0]
-
-validation_set = tn.InputsPreparation(validation_sims, load_ids=True, scaler_output=training_set.labels_scaler,
-                                      random_subset_each_sim=4000)
-generator_validation2 = tn.DataGenerator(validation_set.particle_IDs, validation_set.labels_particle_IDS, s.sims_dic,
-                                        batch_size=4000, rescale_mean=rescale_mean, rescale_std=rescale_std)
-X_val, y_val = generator_validation2[0]
-
-Model2 = CNN.CNN(param_conv, param_fcc, dim=(51, 51, 51),
-                training_generator=generator_training, validation_generator=None, validation_freq=1,
-                callbacks=callbacks_list, use_multiprocessing=True, num_epochs=100,
-                workers=12, verbose=1, model_type="regression", lr=0.0001, train=False)
-
-history = Model2.model.fit(X_train, y_train, batch_size=80, verbose=1, epochs=100, validation_data=(X_val, y_val),
-                          shuffle=True, callbacks=callbacks_list)
-
-
-
-####### tests
-
-s = tn.SimulationPreparation(["0", "1"])
-
-def run():
-    t0 = time.time()
-    training_sims = ["0", "1"]
-    training_set = tn.InputsPreparation(training_sims, load_ids=True)
-    generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
-                                         batch_size=10000, rescale_mean=0, rescale_std=1)
-    x, y = generator_training[0]
-    t1 = time.time()
-    print("Loading 10000 particles took " + str((t1 - t0) / 60))
-
-run()
-
-
-def run2():
-    all_sims = ["0", "1", "2", "4", "5", "6"]
-    s = tn.SimulationPreparation(all_sims)
-
-    t0 = time.time()
-    training_sims = ["0", "2", "4", "5", "6"]
-    training_set = tn.InputsPreparation(training_sims, load_ids=True)
-    generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
-                                          batch_size=100000, rescale_mean=0, rescale_std=1)
-    x, y = generator_training[0]
-    t1 = time.time()
-    print("Loading 100000 particles took " + str((t1 - t0) / 60))
-
-
-run2()
+# training_set = tn.InputsPreparation(training_sims, load_ids=True)
+# generator_training2 = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
+#                                       batch_size=100000, rescale_mean=rescale_mean, rescale_std=rescale_std)
+# X_train, y_train = generator_training2[0]
+#
+# validation_set = tn.InputsPreparation(validation_sims, load_ids=True, scaler_output=training_set.labels_scaler,
+#                                       random_subset_each_sim=4000)
+# generator_validation2 = tn.DataGenerator(validation_set.particle_IDs, validation_set.labels_particle_IDS, s.sims_dic,
+#                                         batch_size=4000, rescale_mean=rescale_mean, rescale_std=rescale_std)
+# X_val, y_val = generator_validation2[0]
+#
+# Model2 = CNN.CNN(param_conv, param_fcc, dim=(51, 51, 51),
+#                 training_generator=generator_training, validation_generator=None, validation_freq=1,
+#                 callbacks=callbacks_list, use_multiprocessing=True, num_epochs=100,
+#                 workers=12, verbose=1, model_type="regression", lr=0.0001, train=False)
+#
+# history = Model2.model.fit(X_train, y_train, batch_size=80, verbose=1, epochs=100, validation_data=(X_val, y_val),
+#                           shuffle=True, callbacks=callbacks_list)
+#
+#
+#
+# ####### tests
+#
+# s = tn.SimulationPreparation(["0", "1"])
+#
+# def run():
+#     t0 = time.time()
+#     training_sims = ["0", "1"]
+#     training_set = tn.InputsPreparation(training_sims, load_ids=True)
+#     generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
+#                                          batch_size=10000, rescale_mean=0, rescale_std=1)
+#     x, y = generator_training[0]
+#     t1 = time.time()
+#     print("Loading 10000 particles took " + str((t1 - t0) / 60))
+#
+# run()
+#
+#
+# def run2():
+#     all_sims = ["0", "1", "2", "4", "5", "6"]
+#     s = tn.SimulationPreparation(all_sims)
+#
+#     t0 = time.time()
+#     training_sims = ["0", "2", "4", "5", "6"]
+#     training_set = tn.InputsPreparation(training_sims, load_ids=True)
+#     generator_training = tn.DataGenerator(training_set.particle_IDs, training_set.labels_particle_IDS, s.sims_dic,
+#                                           batch_size=100000, rescale_mean=0, rescale_std=1)
+#     x, y = generator_training[0]
+#     t1 = time.time()
+#     print("Loading 100000 particles took " + str((t1 - t0) / 60))
+#
+#
+# run2()
 
 
