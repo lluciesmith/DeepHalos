63b4adcdccdee8bd2ce03f847e23897aca0a9daa
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-02-27 10:02:54 +0000

    res 75

--- scripts/regression/train_ics_res75.py
+++ scripts/regression/train_ics_res75.py
@@ -59,7 +59,7 @@ param_conv = {'conv_1': {'num_kernels': 4, 'dim_kernel': (3, 3, 3),
                          'strides': 1, 'padding': 'same', 'pool': "max", 'bn': True},
               'conv_4': {'num_kernels': 32, 'dim_kernel': (3, 3, 3),
                          'strides': 1, 'padding': 'same', 'pool': "max", 'bn': True},
-              'conv_5': {'num_kernels': 4, 'dim_kernel': (1, 1, 1),
+              'conv_5': {'num_kernels': 8, 'dim_kernel': (1, 1, 1),
                          'strides': 1, 'padding': 'same', 'pool': None, 'bn': True}
               }
 
@@ -71,7 +71,7 @@ Model = CNN.CNN(param_conv, param_fcc, dim=dim,
                 training_generator=generator_training, validation_generator=generator_validation, validation_freq=1,
                 callbacks=callbacks_list, num_epochs=100,
                 use_multiprocessing=True, workers=2, max_queue_size=10,
-                verbose=1, model_type="regression", lr=0.001, train=True)
+                verbose=1, model_type="regression", lr=0.0001, train=True)
 
 
 np.save(path_model + "/history_100_epochs_mixed_sims.npy", Model.history)
