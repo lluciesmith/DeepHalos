a069da2de8ccc29407769053e026b03525c65366
Luisa Lucie-Smith <lls@chewbacca.star.ucl.ac.uk>
2020-03-04 10:46:48 +0000

    no shuffling

--- scripts/regression/predictions.py
+++ scripts/regression/predictions.py
@@ -31,7 +31,7 @@ dim = (75, 75, 75)
 s = tn.SimulationPreparation(validation_sims)
 scaler_output = load(open(path_model + 'scaler_output.pkl', 'rb'))
 
-validation_set = tn.InputsPreparation(validation_sims, load_ids=True, scaler_output=scaler_output)
+validation_set = tn.InputsPreparation(validation_sims, load_ids=True, scaler_output=scaler_output, shuffle=False)
 generator_validation = tn.DataGenerator(validation_set.particle_IDs, validation_set.labels_particle_IDS, s.sims_dic,
                                         batch_size=batch_size, rescale_mean=rescale_mean, rescale_std=rescale_std)
 
