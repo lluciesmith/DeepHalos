82b6f54f7af8386c8f920d036d716b466aacda3b
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-03-09 10:23:00 +0000

    predict 121^3 run with stride 1

--- scripts/regression/predictions.py
+++ scripts/regression/predictions.py
@@ -23,8 +23,8 @@ def get_sim_predictions_given_model(sims, model, scaler_output, val_sim="1",
     true = scaler_output.inverse_transform(truth_rescaled).flatten()
 
     if save is True:
-        np.save(path_model + "predictions/predicted" + val_sim + "_" + num_epoch + ".npy", h_m_pred)
-        np.save(path_model + "predictions/true" + val_sim + "_" + num_epoch + ".npy", true)
+        np.save(path_model + "predicted" + val_sim + "_" + num_epoch + ".npy", h_m_pred)
+        np.save(path_model + "true" + val_sim + "_" + num_epoch + ".npy", true)
 
     return h_m_pred, true
 
@@ -35,31 +35,28 @@ if __name__ == "__main__":
     # First choose the correct path to the model and the parameters you used during training
 
     params_model = {# 'path_model':"/lfstev/deepskies/luisals/regression/rolling_val/",
-                    'path_model': "/lfstev/deepskies/luisals/regression/train_mixed_sims/51_3_maxpool/",
-                    'num_epoch': "100",
-                    'batch_size': 80,
+                    #'path_model': "/lfstev/deepskies/luisals/regression/train_mixed_sims/51_3_maxpool/",
+                    'path_model': "/lfstev/deepskies/luisals/regression/ics_res121/stride1/",
+                    'num_epoch': "30",
+                    'batch_size': 40,
                     'rescale_mean': 1.005,
                     'rescale_std': 0.05050,
-                    'dim': (51, 51, 51)
+                    'dim': (121, 121, 121)
                     }
 
     # load validation sets
 
-    validation_sims = ["6", "1", "0"]
+    validation_sims = ["1"]
     s = tn.SimulationPreparation(validation_sims)
-    s_output = load(open('/lfstev/deepskies/luisals/regression/rolling_val/scaler_output.pkl', 'rb'))
-    # s_output = load(open(params_model['path_model'] + 'scaler_output.pkl', 'rb'))
+    s_output = load(open(params_model['path_model'] + 'scaler_output.pkl', 'rb'))
 
     # load model
 
-    model_roll = load_model(params_model['path_model'] + "/model_100_epochs_mixed_sims.h5")
-    # model = load_model(path_model + "model/weights." + num_epoch + ".hdf5")
+    # model_roll = load_model(params_model['path_model'] + "/model_100_epochs_mixed_sims.h5")
+    model_epoch = load_model(params_model['path_model'] + "model/weights." + params_model['num_epoch'] + ".hdf5")
 
-    p6, t6 = get_sim_predictions_given_model(s, model_roll, s_output, val_sim="6", **params_model)
-    p1, t1 = get_sim_predictions_given_model(s, model_roll, s_output, val_sim="1", **params_model)
-    p0, t0 = get_sim_predictions_given_model(s, model_roll, s_output, val_sim="0", **params_model)
+    for val_sim in validation_sims:
+        pi, ti = get_sim_predictions_given_model(s, model_epoch, s_output, val_sim="1", **params_model)
+        print(np.corrcoef(ti, pi))
 
-    print(np.corrcoef(t6, p6))
-    print(np.corrcoef(t1, p1))
-    print(np.corrcoef(t0, p0))
 
