d5b32c92e00cf26aac108eb76c8e943410012c05
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-03-10 13:13:06 +0000

    hm

--- features/ordering_halo_mass_particles.py
+++ features/ordering_halo_mass_particles.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys; sys.path.append("/home/lls/mlhalos_code/")
 from mlhalos import parameters
 import pynbody
+import time
 
 
 if __name__ == "__main__":
@@ -16,8 +17,12 @@ if __name__ == "__main__":
         f.physical_units()
 
         # get halo masses each particles
-
-        h = f.halos(order=False, make_grp=True)
+        print("Loading the halos...")
+        t0 = time.time()
+        h = f.halos(make_grp=True)
+        t1 = time.time()
+        print("Loading halos took " + str((t1 - t0)/60) + " minutes.")
+        print("Done loading halos.")
         h_id = f['grp']
 
         halo_ids = np.arange(len(h))
