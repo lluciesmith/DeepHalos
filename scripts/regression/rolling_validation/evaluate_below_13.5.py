28ad9f3570d7832953e0db355d8e03b9be9f82c2
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-03-18 17:51:06 +0000

    evaluate

--- scripts/regression/rolling_validation/evaluate_below_13.5.py
+++ scripts/regression/rolling_validation/evaluate_below_13.5.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.path.append("/home/luisals/DeepHalos")
 from tensorflow.keras.models import load_model
 import dlhalos_code.data_processing as tn
@@ -10,13 +9,13 @@ if __name__ == "__main__":
     ########## CREATE GENERATORS FOR TRAINING AND VALIDATION #########
 
     # First choose the correct path to the model and the parameters you used during training
-    params_model0 = {'path_model': "/lfstev/deepskies/luisals/regression/rolling_val/no_sim3_w_eval_0dropout/"}
+    # params_model0 = {'path_model': "/lfstev/deepskies/luisals/regression/rolling_val/no_sim3_w_eval_0dropout/"}
     params_model01 = {'path_model': "/lfstev/deepskies/luisals/regression/rolling_val/no_sim3_w_eval_0.1dropout/"}
     params_model02 = {'path_model': "/lfstev/deepskies/luisals/regression/rolling_val/no_sim3_w_eval_0.2dropout/"}
     params_model03 = {'path_model': "/lfstev/deepskies/luisals/regression/rolling_val/no_sim3_w_eval_0.3dropout/"}
     params_model04 = {'path_model': "/lfstev/deepskies/luisals/regression/rolling_val/no_sim3/"}
 
-    for params_model in [params_model0, params_model01, params_model02, params_model03, params_model04]:
+    for params_model in [params_model01, params_model02, params_model03, params_model04]:
 
         params_inputs = {'batch_size': 80,
                          'rescale_mean': 1.005,
@@ -91,4 +90,3 @@ if __name__ == "__main__":
             del model_epoch
 
         np.save(params_model['path_model'] + "loss_training_sim7_validaton_above_135Msol.npy", loss)
-ev
