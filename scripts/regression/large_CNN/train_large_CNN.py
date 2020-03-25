c912227ee815d9e305934fffd432e7d7e1639994
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-03-25 23:55:55 +0000

    large CNN

--- scripts/regression/large_CNN/train_large_CNN.py
+++ scripts/regression/large_CNN/train_large_CNN.py
@@ -60,10 +60,10 @@ if __name__ == "__main__":
     csv_logger = CSVLogger(path_model + "/training.log", separator=',')
 
     # tensorboard
-    # tb = TensorBoard(log_dir=path_model + '/logs', histogram_freq=1, write_grads=True)
+    tb = TensorBoard(log_dir=path_model + '/logs', histogram_freq=1, write_grads=True)
 
-    # callbacks_list = [checkpoint_call, csv_logger, tb]
-    callbacks_list = [checkpoint_call, csv_logger]
+    callbacks_list = [checkpoint_call, csv_logger, tb]
+    # callbacks_list = [checkpoint_call, csv_logger]
     tensorflow.compat.v1.set_random_seed(7)
 
     kernel_reg = regularizers.l2(0.0005)
