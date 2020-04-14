07ae89fd89eeec5b18d8d37bfecd56571b5768d4
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-04-14 14:48:20 +0100

    custom loss

--- scripts/scratch/test_gradients/test_lowmass_custom.py
+++ scripts/scratch/test_gradients/test_lowmass_custom.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
                     training_generator=generator_training, validation_generator=generator_validation,
                     lr=lr, callbacks=callbacks_list, metrics=['mae', 'mse'],
                     num_epochs=100, dim=params_inputs['dim'], loss=custom_loss,
-                    max_queue_size=1, use_multiprocessing=False, workers=2, verbose=1,
+                    max_queue_size=10, use_multiprocessing=True, workers=2, verbose=1,
                     num_gpu=1, save_summary=True,  path_summary=path_model, validation_freq=1, train=True,
                     compile=True)
 
