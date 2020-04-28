1eb56ea009046f2156c07fb504f49c4aefc99900
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-04-28 15:06:41 +0100

    evaluate

--- scripts/scratch/test_gradients/losses/evaluate_models.py
+++ scripts/scratch/test_gradients/losses/evaluate_models.py
@@ -13,28 +13,6 @@ from tensorflow.keras.models import load_model
 import gc
 import tensorflow as tf
 
-
-def predict_from_model(model, epoch, gen_train, gen_val,
-                       training_IDs, training_labels_IDS, val_IDs, val_labels_IDs, scaler,path_model):
-
-    pred = model.predict_generator(gen_train, use_multiprocessing=False, workers=1, verbose=1, max_queue_size=10)
-    truth_rescaled = np.array([training_labels_IDS[ID] for ID in training_IDs])
-
-    h_m_pred = scaler.inverse_transform(pred.reshape(-1, 1)).flatten()
-    true = scaler.inverse_transform(truth_rescaled.reshape(-1, 1)).flatten()
-
-    np.save(path_model + "predicted_training_"+ epoch + ".npy", h_m_pred)
-    np.save(path_model + "true_training_"+ epoch + ".npy", true)
-
-    pred = model.predict_generator(gen_val, use_multiprocessing=False, workers=1, verbose=1, max_queue_size=10)
-    truth_rescaled = np.array([val_labels_IDs[ID] for ID in val_IDs])
-
-    h_m_pred = scaler.inverse_transform(pred.reshape(-1, 1)).flatten()
-    true = scaler.inverse_transform(truth_rescaled.reshape(-1, 1)).flatten()
-
-    np.save(path_model + "predicted_val_"+ epoch + ".npy", h_m_pred)
-    np.save(path_model + "true_val_" + epoch + ".npy", true)
-
 if __name__ == "__main__":
 
     ########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
@@ -64,27 +42,27 @@ if __name__ == "__main__":
     path = "/lfstev/deepskies/luisals/regression/large_CNN/test_lowmass/reg_10000_perbin/larger_net/lr_decay/"
 
     epochs = ["%.2s" % i for i in np.arange(15, 75, 5)]
-    epochs.append("100")
 
     # MODEL CAUCHY + SELEC + BOUNDARY
-
-    model = path + "cauchy_selec_bound/"
-    eval = open(model + "val_callback.txt", "w")
-
-    with tf.device("/cpu:0"):
-        for epoch in epochs[2:]:
-                model_epoch = load_model(model + "model/weights." + epoch + ".hdf5",
-                                   custom_objects={'loss':lf.cauchy_selection_loss_fixed_boundary()})
-
-                l = model_epoch.evaluate(x=generator_validation, verbose=1, workers=0, steps=50)
-
-                eval.write(epoch + ", ")
-                for item in l:
-                    eval.write("%s, " % item)
-                eval.write(" \n")
-
-                del model_epoch
-                gc.collect()
+    #
+    # model = path + "cauchy_selec_bound/"
+    # eval = open(model + "val_callback.txt", "w")
+    # eval.write("Epoch, val_loss, val_mae, val_mse \n")
+    #
+    # with tf.device("/cpu:0"):
+    #     for epoch in epochs:
+    #             model_epoch = load_model(model + "model/weights." + epoch + ".hdf5",
+    #                                custom_objects={'loss':lf.cauchy_selection_loss_fixed_boundary()})
+    #
+    #             l = model_epoch.evaluate(x=generator_validation, verbose=1, workers=0, steps=50)
+    # 
+    #             eval.write(epoch + ", ")
+    #             for item in l:
+    #                 eval.write("%s, " % item)
+    #             eval.write(" \n")
+    #
+    #             del model_epoch
+    #             gc.collect()
 
     # MODEL CAUCHY + SELEC
 
@@ -93,7 +71,7 @@ if __name__ == "__main__":
     eval.write("Epoch, val_loss, val_mae, val_mse \n")
 
     with tf.device("/cpu:0"):
-        for epoch in epochs[2:]:
+        for epoch in epochs:
                 model_epoch = load_model(model + "model/weights." + epoch + ".hdf5",
                                    custom_objects={'loss':lf.cauchy_selection_loss()})
 
@@ -106,10 +84,4 @@ if __name__ == "__main__":
 
                 del model_epoch
                 gc.collect()
-
-    # MODEL CAUCHY + SELEC + GAMMA TRAINABLE
-
-
-
-    # model = path + "cauchy_selec_gamma/"
 
