79424054ad3f1e2c05b0b1ee363572a77a01a30e
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-05-15 11:40:39 +0100

    reg

--- dlhalos_code/regularizers.py
+++ dlhalos_code/regularizers.py
@@ -15,11 +15,27 @@ def l1_norm(alpha):
 
 
 def l1_and_l21_group(alpha):
-    return K.sum(l1_norm(alpha), l21_group(alpha))
+    return L21_and_L1(alpha)
 
 
 def l21_group(alpha):
     return L21(alpha)
+
+
+class L21_and_L1(Regularizer):
+    """ Regularizer for combined L21 group regularization and L1 regularization. """
+
+    def __init__(self, alpha=0.):
+        self.alpha = K.cast_to_floatx(alpha)
+
+    def __call__(self, x):
+        regularization = 0.
+        regularization += self.alpha * K.sum(K.abs(x))
+        regularization += self.alpha * K.sum(K.sqrt(K.sum(K.square(x), axis=1)))
+        return regularization
+
+    def get_config(self):
+        return {'alpha': self.alpha}
 
 
 class L21(Regularizer):
@@ -34,6 +50,9 @@ class L21(Regularizer):
 
         reg = self.alpha * K.sum(K.sqrt(K.sum(K.square(x), axis=1)))
         return reg
+
+    def get_config(self):
+        return {'alpha': self.alpha}
 
 
 # Utility function to count active neurons in a Keras model with Dense layers
