603c7ad95c0352dd04371490062c3bac0a2c4df7
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-05-19 13:39:13 +0100

    alpha

--- scripts/scratch/test_gradients/losses/cauchy_selec_bound_train_gamma/train_reg_alpha.py
+++ scripts/scratch/test_gradients/losses/cauchy_selec_bound_train_gamma/train_reg_alpha.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
                           training_generator=generator_training, validation_generator=generator_validation,
                           num_epochs=60, validation_freq=1, lr=0.0001, max_queue_size=10, use_multiprocessing=False,
                           workers=0, verbose=1, num_gpu=1, save_summary=True, path_summary=path, compile=True,
-                          train=True, load_mse_weights=True, **reg_params)
+                          train=True, load_mse_weights=False, **reg_params)
 
     # Model = CNN.CNNCauchy(param_conv, param_fcc, model_type="regression",
     #                       training_generator={}, validation_generator={}, dim=(31, 31, 31),
