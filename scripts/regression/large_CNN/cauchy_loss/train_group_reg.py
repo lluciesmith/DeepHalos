917295d661b6cac7ba404b5a4087c53679550e01
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-06-11 18:09:53 +0200

    cauchy

--- scripts/regression/large_CNN/cauchy_loss/train_group_reg.py
+++ scripts/regression/large_CNN/cauchy_loss/train_group_reg.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
                       model_type="regression", dim=generator_training.dim,
                       training_generator=generator_training, validation_generator=generator_validation,
                       num_epochs=60, validation_freq=1, max_queue_size=10, use_multiprocessing=False,
-                      workers=0, verbose=1, num_gpu=4, save_summary=True, path_summary=path_model,
+                      workers=0, verbose=1, num_gpu=1, save_summary=True, path_summary=path_model,
                       compile=True, train=True, load_weights=None,
                       load_mse_weights=False, use_mse_n_epoch=3, use_tanh_n_epoch=0,
                       **reg_params)
