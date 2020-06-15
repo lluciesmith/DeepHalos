a01d62df5308b81cad2834d8dee4f6b9a2437404
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-06-15 09:57:03 +0200

    low mass training set on Pearl

--- scripts/scratch/test_gradients/losses/training_set.py
+++ scripts/scratch/test_gradients/losses/training_set.py
@@ -5,15 +5,19 @@ from pickle import dump
 
 if __name__ == "__main__":
 
-    path_data = "/lfstev/deepskies/luisals/regression/large_CNN/test_lowmass/reg_10000_perbin/larger_net/"
+    # path_data = "/lfstev/deepskies/luisals/regression/large_CNN/test_lowmass/reg_10000_perbin/larger_net/"
+    path_data = "/mnt/beegfs/work/ati/pearl037/regression/low_mass/"
+
 
     # First you will have to load the simulation
 
     all_sims = ["0", "1", "2", "4", "5", "6"]
-    s = tn.SimulationPreparation(all_sims)
+    s = tn.SimulationPreparation(all_sims, path="/mnt/beegfs/work/ati/pearl037/")
 
     train_sims = all_sims[:-1]
     val_sim = all_sims[-1]
+
+    # training sets
 
     training_set = tn.InputsPreparation(train_sims, scaler_type="minmax",
                                         load_ids=False, shuffle=True, log_high_mass_limit=13,
@@ -23,9 +27,27 @@ if __name__ == "__main__":
     dump(training_set.labels_particle_IDS, open(path_data + 'labels_training_set.pkl', 'wb'))
     dump(training_set.scaler_output, open(path_data + 'scaler_output.pkl', 'wb'))
 
+    training_set = tn.InputsPreparation(train_sims, shuffle=True, scaler_type="minmax", load_ids=False,
+                                        log_high_mass_limit=13,
+                                        random_subset_each_sim=1000000, random_style="uniform",
+                                        num_per_mass_bin=1000, num_bins=50)
+
+    dump(training_set.particle_IDs, open(path_data + 'training_set_50000.pkl', 'wb'))
+    dump(training_set.labels_particle_IDS, open(path_data + 'labels_training_set_50000.pkl', 'wb'))
+    dump(training_set.scaler_output, open(path_data + 'scaler_output_50000.pkl', 'wb'))
+
+    # validation sets
+
     v_set = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, shuffle=True, log_high_mass_limit=13,
                                  random_style="random", random_subset_each_sim=5000,
                                  scaler_output=training_set.scaler_output)
     dump(v_set.particle_IDs, open(path_data + 'validation_set.pkl', 'wb'))
     dump(v_set.labels_particle_IDS, open(path_data + 'labels_validation_set.pkl', 'wb'))
+
+    v_set2 = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, log_high_mass_limit=13,
+                                 random_style="random", random_subset_each_sim=50000,
+                                 scaler_output=training_set.scaler_output)
+    dump(v_set2.particle_IDs, open(path_data + 'larger_validation_set.pkl', 'wb'))
+    dump(v_set2.labels_particle_IDS, open(path_data + 'larger_labels_validation_set.pkl', 'wb'))
+
 
