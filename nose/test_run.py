ba8f5d9067e3319d9330694136ebf42018ee1a82
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-06-18 11:35:02 +0200

    test

--- nose/test_run.py
+++ nose/test_run.py
@@ -15,7 +15,7 @@ np.random.seed(123)
 python_random.seed(123)
 tf.compat.v1.set_random_seed(1234)
 
-pearl = True
+pearl = False
 
 if pearl:
     path = "/mnt/beegfs/work/ati/pearl037/regression/test/"
@@ -35,53 +35,54 @@ l_ids = load(open(path + 'labels_training_set.pkl', 'rb'))
 v_ids = load(open(path + 'validation_set.pkl', 'rb'))
 l_v_ids = load(open(path + 'labels_validation_set.pkl', 'rb'))
 
-dim = (51, 51, 51)
-params_tr = {'batch_size': 100, 'rescale_mean': 1.005, 'rescale_std': 0.05050, 'dim': dim}
-generator_training = tn.DataGenerator(p_ids, l_ids, s.sims_dic, shuffle=False, **params_tr)
-generator_validation = tn.DataGenerator(v_ids, l_v_ids, s.sims_dic, shuffle=False, **params_tr)
+for i in range(2):
+    dim = (51, 51, 51)
+    params_tr = {'batch_size': 100, 'rescale_mean': 1.005, 'rescale_std': 0.05050, 'dim': dim}
+    generator_training = tn.DataGenerator(p_ids, l_ids, s.sims_dic, shuffle=False, **params_tr)
+    generator_validation = tn.DataGenerator(v_ids, l_v_ids, s.sims_dic, shuffle=False, **params_tr)
 
-# Convolutional layers parameters
+    # Convolutional layers parameters
 
-params_all_conv = {'activation': "linear", 'relu': True, 'strides': 1, 'padding': 'same', 'bn': False,
-                   'kernel_regularizer': reg.l2_norm(10**-3.5)}
-param_conv = {'conv_1': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
-              'conv_2': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-              'conv_3': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-              'conv_4': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-              'conv_5': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-              }
+    params_all_conv = {'activation': "linear", 'relu': True, 'strides': 1, 'padding': 'same', 'bn': False,
+                       'kernel_regularizer': reg.l2_norm(10**-3.5)}
+    param_conv = {'conv_1': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
+                  'conv_2': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_3': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_4': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_5': {'num_kernels': 2, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  }
 
-# Dense layers parameters
+    # Dense layers parameters
 
-params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True,
-                  'kernel_regularizer': reg.l2_norm(10**-3.5)}
-param_fcc = {'dense_1': {'neurons': 256, **params_all_fcc}, 'dense_2': {'neurons': 128, **params_all_fcc},
-             'last': {}}
+    params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True,
+                      'kernel_regularizer': reg.l2_norm(10**-3.5)}
+    param_fcc = {'dense_1': {'neurons': 256, **params_all_fcc}, 'dense_2': {'neurons': 128, **params_all_fcc},
+                 'last': {}}
 
-# Regularization parameters + Cauchy likelihood
+    # Regularization parameters + Cauchy likelihood
 
-reg_params = {'init_gamma': 0.2}
+    reg_params = {'init_gamma': 0.2}
 
-# for i in range(10):
-#     np.random.seed(123)
-#     python_random.seed(123)
-#     tf.compat.v1.set_random_seed(1234)
-#
-#     try:
-#         os.mkdir(path + 'run_' + str(i))
-#     except:
-#         pass
-#     path1 = path + 'run_' + str(i) + '/'
-Model = CNN.CNN(param_conv, param_fcc, model_type="regression", dim=generator_training.dim,
-                training_generator=generator_training, validation_generator=generator_validation,
-                num_epochs=10, validation_freq=1, lr=0.0001, max_queue_size=10,
-                use_multiprocessing=False,
-                workers=0, verbose=1, num_gpu=1, save_summary=True, path_summary=path,
-                compile=True, train=True,
-                # load_weights=None, load_mse_weights=False, use_mse_n_epoch=10, use_tanh_n_epoch=0,
-                # reg_params
-                )
-# del Model
+    # for i in range(10):
+    #     np.random.seed(123)
+    #     python_random.seed(123)
+    #     tf.compat.v1.set_random_seed(1234)
+    #
+    #     try:
+    #         os.mkdir(path + 'run_' + str(i))
+    #     except:
+    #         pass
+    #     path1 = path + 'run_' + str(i) + '/'
+    Model = CNN.CNN(param_conv, param_fcc, model_type="regression", dim=generator_training.dim,
+                    training_generator=generator_training, validation_generator=generator_validation,
+                    num_epochs=10, validation_freq=1, lr=0.0001, max_queue_size=10,
+                    use_multiprocessing=False,
+                    workers=0, verbose=1, num_gpu=1, save_summary=True, path_summary=path,
+                    compile=True, train=True,
+                    # load_weights=None, load_mse_weights=False, use_mse_n_epoch=10, use_tanh_n_epoch=0,
+                    # reg_params
+                    )
+    # del Model
 
 
 
