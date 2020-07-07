440a2fdb04a7fd44276111afc9cf4a01aec4db6e
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-07-07 09:45:07 +0200

    train group reg

--- scripts/regression/full_mass_range/train_group_reg.py
+++ scripts/regression/full_mass_range/train_group_reg.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append("/home/luisals/DeepHalos")
 from dlhalos_code import CNN
 from dlhalos_code import custom_regularizers as reg
 import dlhalos_code.data_processing as tn
@@ -11,41 +9,31 @@ import random as python_random
 
 if __name__ == "__main__":
 
-    pearl = sys.argv[1]
-    print(pearl)
     ########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
 
-    # np.random.seed(123)
-    # python_random.seed(123)
-    # tf.compat.v1.set_random_seed(1234)
+    saving_path = "/mnt/beegfs/work/ati/pearl037/regression/full_mass_range/200k_random_training/test/"
+
+    seed = 123
+    np.random.seed(seed)
+    python_random.seed(seed)
+    tf.compat.v1.set_random_seed(seed)
 
     # Load data
-    if pearl == "pearl":
-        path_data = "/mnt/beegfs/work/ati/pearl037/regression/full_mass_range_51_3_fermi/"
-        saving_path = "/mnt/beegfs/work/ati/pearl037/regression/full_mass_range_51_3_fermi/diff_seeds2/"
-        path_sims = "/mnt/beegfs/work/ati/pearl037/"
 
-    else:
-        path_data = "/lfstev/deepskies/luisals/regression/large_CNN/test_lowmass/reg_10000_perbin/larger_net/" \
-                    "lr_decay/cauchy_selec_bound_gamma_train_alpha/full_mass_range/"
-        saving_path = path_data + "test/diff_seeds2/"
-        path_sims = "/lfstev/deepskies/luisals/"
-
-    path = "/mnt/beegfs/work/ati/pearl037/regression/test/fermi_training/"
-
+    path_sims = "/mnt/beegfs/work/ati/pearl037/"
     all_sims = ["0", "1", "2", "4", "5", "6"]
     s = tn.SimulationPreparation(all_sims, path=path_sims)
 
+    path_data = "/mnt/beegfs/work/ati/pearl037/regression/training_set/random/200k/"
     training_particle_IDs = load(open(path_data + 'training_set.pkl', 'rb'))
     training_labels_particle_IDS = load(open(path_data + 'labels_training_set.pkl', 'rb'))
-
     val_particle_IDs = load(open(path_data + 'validation_set.pkl', 'rb'))
     val_labels_particle_IDS = load(open(path_data + 'labels_validation_set.pkl', 'rb'))
 
     # Create the generators for training
 
-    dim = (51, 51, 51)
-    params_tr = {'batch_size': 100, 'rescale_mean': 1.005, 'rescale_std': 0.05050, 'dim': dim}
+    dim = (75, 75, 75)
+    params_tr = {'batch_size': 64, 'rescale_mean': 1.005, 'rescale_std': 0.05050, 'dim': dim}
     generator_training = tn.DataGenerator(training_particle_IDs, training_labels_particle_IDS, s.sims_dic,
                                           shuffle=True, **params_tr)
     generator_validation = tn.DataGenerator(val_particle_IDs, val_labels_particle_IDS, s.sims_dic,
@@ -56,20 +44,22 @@ if __name__ == "__main__":
 
     alpha = 10**(-3.5)
     params_all_conv = {'activation': "linear", 'relu': True, 'strides': 1, 'padding': 'same', 'bn': False,
-                       'kernel_regularizer': reg.l2_norm(alpha)}
+                       'kernel_regularizer': reg.l2_norm(alpha)
+                       }
     param_conv = {'conv_1': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
                   'conv_2': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
                   'conv_3': {'num_kernels': 64, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
                   'conv_4': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
                   'conv_5': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-                  'conv_6': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_6': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv}
                   }
     # Added conv_6 in going from 31^3 input to 75^3 input
 
     # Dense layers parameters
 
     params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True,
-                      'kernel_regularizer': reg.l1_and_l21_group(alpha)}
+                      'kernel_regularizer': reg.l1_and_l21_group(alpha)
+                      }
     param_fcc = {'dense_1': {'neurons': 256, **params_all_fcc}, 'dense_2': {'neurons': 128, **params_all_fcc},
                  'last': {}}
 
@@ -77,12 +67,23 @@ if __name__ == "__main__":
 
     # Train for one epoch using MSE loss
 
+    weights = saving_path + "model/weights.03.h5"
     Model = CNN.CNNCauchy(param_conv, param_fcc, lr=0.0001, model_type="regression", shuffle=True,
                       dim=generator_training.dim, training_generator=generator_training,
-                      validation_generator=generator_validation, num_epochs=60, validation_freq=1,
-                      max_queue_size=10, use_multiprocessing=False,  workers=0, verbose=1, num_gpu=1,
-                      save_summary=True, path_summary=saving_path, seed=None,
-                      compile=True, train=True, load_weights=None,
-                      load_mse_weights=False, use_mse_n_epoch=1, use_tanh_n_epoch=0, **reg_params)
+                      validation_generator=generator_validation, num_epochs=100, validation_freq=1,
+                      max_queue_size=80, use_multiprocessing=True,  workers=40, verbose=1, num_gpu=1,
+                      save_summary=True, path_summary=saving_path, seed=seed, initial_epoch=3,
+                      compile=True, train=True, load_weights=weights, alpha_mse=10**-4,
+                      load_mse_weights=True, use_mse_n_epoch=1, use_tanh_n_epoch=0,
+                    **reg_params
+    )
+
+# Model = CNN.CNN(param_conv, param_fcc, lr=0.0001, model_type="regression", shuffle=True,
+#                   dim=generator_training.dim, training_generator=generator_training,
+#                   validation_generator=generator_validation, num_epochs=100, validation_freq=1,
+#                   max_queue_size=10, use_multiprocessing=True,  workers=0, verbose=1, num_gpu=2,
+#                   save_summary=True, path_summary=saving_path, seed=seed,
+#                   compile=True, train=True, loss="mse")
+
 
 
