75e25e33e9604bcc1e7f4e5c2134884054df8cb4
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-07-17 14:48:21 +0200

    Two more runs: one with BN layer and one with larger learning rate (w MSE epoch)

--- scripts/regression/full_mass_range/smaller_network_lr_5e-3.py
+++ scripts/regression/full_mass_range/smaller_network_lr_5e-3.py
@@ -73,6 +73,6 @@ if __name__ == "__main__":
                           lr=0.005, save_summary=True,
                           path_summary=saving_path, validation_freq=1, train=True, compile=True,
                           seed=seed, global_average=True,
-                          use_mse_n_epoch=1, load_mse_weights=False)
+                          use_mse_n_epoch=1, load_mse_weights=True)
 
 
