e174da4434300d4e7bca21253fb1ac08e1c0b859
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-07-22 10:52:29 +0200

    alpha -3

--- scripts/regression/full_mass_range/train_xavier_9sims.py
+++ scripts/regression/full_mass_range/train_xavier_9sims.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
 ########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
 
-    saving_path = "/mnt/beegfs/work/ati/pearl037/regression/full_mass_range/200k_random_training/9sims/Xavier/narrow/"
+    saving_path = "/mnt/beegfs/work/ati/pearl037/regression/full_mass_range/200k_random_training/9sims/Xavier/alpha-3/"
 
     seed = 123
     np.random.seed(seed)
@@ -43,30 +43,31 @@ if __name__ == "__main__":
 
     ######### TRAIN THE MODEL ################
 
+    alpha = 10**-3
     params_all_conv = {'activation': "linear", 'relu': True, 'strides': 1, 'padding': 'same', 'bn': False,
-                       'kernel_regularizer': reg.l2_norm(10**(-3.5))
+                       'kernel_regularizer': reg.l2_norm(alpha)
                        }
-    param_conv = {'conv_1': {'num_kernels': 16, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
-                  'conv_2': {'num_kernels': 16, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-                  'conv_3': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-                  'conv_4': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-                  'conv_5': {'num_kernels': 64, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
-                  'conv_6': {'num_kernels': 64, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv}
+    param_conv = {'conv_1': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
+                  'conv_2': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_3': {'num_kernels': 64, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_4': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_5': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+                  'conv_6': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv}
                   }
     # Added conv_6 in going from 31^3 input to 75^3 input
 
     # Dense layers parameters
 
     params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True,
-                      'kernel_regularizer': reg.l1_and_l21_group(10**(-4.5))}
+                      'kernel_regularizer': reg.l1_and_l21_group(alpha)}
     param_fcc = {'dense_1': {'neurons': 256, **params_all_fcc}, 'dense_2': {'neurons': 128, **params_all_fcc},
                  'last': {}}
 
     Model = CNN.CNNCauchy(param_conv, param_fcc, model_type="regression", training_generator=generator_training,
                           shuffle=True, validation_generator=generator_validation, num_epochs=30,
                           metrics=[CNN.likelihood_metric],
-                          dim=generator_training.dim, initialiser="Xavier_uniform", max_queue_size=64,
-                          use_multiprocessing=True, workers=32, verbose=1, num_gpu=1, lr=0.0001, save_summary=True,
+                          dim=generator_training.dim, initialiser="Xavier_uniform", max_queue_size=10,
+                          use_multiprocessing=True, workers=10, verbose=1, num_gpu=1, lr=0.0001, save_summary=True,
                           path_summary=saving_path, validation_freq=1, train=True, compile=True,
                           initial_epoch=None,
                           seed=seed)
