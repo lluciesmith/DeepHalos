8847d5d1dcbe062d343e383f13e19a0ea10f3f34
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-07-28 17:41:43 +0200

    bla

--- scripts/regression/mass_range_13.4/training_set_20sims.py
+++ scripts/regression/mass_range_13.4/training_set_20sims.py
@@ -3,8 +3,8 @@ from pickle import dump
 import numpy as np
 
 if __name__ == "__main__":
-    path_random = "/mnt/beegfs/work/ati/pearl037/regression/training_set_13.4/20sims/random/"
-    path_uniform = "/mnt/beegfs/work/ati/pearl037/regression/training_set_13.4/20sims/uniform/"
+    path_random = "/mnt/beegfs/work/ati/pearl037/regression/training_set/20sims/random/"
+    # path_uniform = "/mnt/beegfs/work/ati/pearl037/regression/training_set/20sims/uniform/"
 
     path_sims = "/mnt/beegfs/work/ati/pearl037/"
     all_sims = ["%i" % i for i in np.arange(22)]
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     saving_path = path_random + "200k/"
 
     training_set = tn.InputsPreparation(train_sims, shuffle=True, scaler_type="minmax", return_rescaled_outputs=True,
-                                        output_range=(-1, 1), log_high_mass_limit=13.4,
+                                        output_range=(-1, 1),
                                         load_ids=False, random_subset_each_sim=None,
                                         random_style="random", random_subset_all=n_samples,
                                         path=path_sims)
@@ -31,13 +31,13 @@ if __name__ == "__main__":
     dump(training_set.scaler_output, open(saving_path + 'scaler_output.pkl', 'wb'))
 
     v_set = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, shuffle=True,
-                                 random_style="random", log_high_mass_limit=13.4,
+                                 random_style="random",
                                  random_subset_all=5000, random_subset_each_sim=1000000,
                                  scaler_output=training_set.scaler_output, path=path_sims)
     dump(v_set.particle_IDs, open(saving_path + 'validation_set.pkl', 'wb'))
     dump(v_set.labels_particle_IDS, open(saving_path + 'labels_validation_set.pkl', 'wb'))
 
-    v_set2 = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, log_high_mass_limit=13.4,
+    v_set2 = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False,
                                  random_style="random", random_subset_all=50000, random_subset_each_sim=1000000,
                                  scaler_output=training_set.scaler_output, path=path_sims)
     dump(v_set2.particle_IDs, open(saving_path + 'larger_validation_set.pkl', 'wb'))
@@ -51,31 +51,31 @@ if __name__ == "__main__":
 
     # Save training sets sampled uniformly from 80 bins, with n=4000 per mass bin
 
-    saving_path = path_uniform + "2.5k_in_each_80bins/"
-    n_samples = 2500
-
-    training_set = tn.InputsPreparation(train_sims, shuffle=True, scaler_type="minmax", return_rescaled_outputs=True,
-                                        output_range=(-1, 1), load_ids=False, random_subset_each_sim=None,
-                                        log_high_mass_limit=13.4,
-                                        random_style="uniform", num_per_mass_bin=n_samples, num_bins=80,
-                                        path=path_sims)
-
-    dump(training_set.particle_IDs, open(saving_path + 'training_set.pkl', 'wb'))
-    dump(training_set.labels_particle_IDS, open(saving_path + 'labels_training_set.pkl', 'wb'))
-    dump(training_set.scaler_output, open(saving_path + 'scaler_output.pkl', 'wb'))
-
-    v_set = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, shuffle=True,
-                                 random_style="random", random_subset_all=5000, random_subset_each_sim=1000000,
-                                 log_high_mass_limit=13.4,
-                                 scaler_output=training_set.scaler_output, path=path_sims)
-    dump(v_set.particle_IDs, open(saving_path + 'validation_set.pkl', 'wb'))
-    dump(v_set.labels_particle_IDS, open(saving_path + 'labels_validation_set.pkl', 'wb'))
-
-    v_set2 = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False,
-                                  log_high_mass_limit=13.4, random_style="random", random_subset_all=50000,
-                                  random_subset_each_sim=1000000,
-                                  scaler_output=training_set.scaler_output, path=path_sims)
-    dump(v_set2.particle_IDs, open(saving_path + 'larger_validation_set.pkl', 'wb'))
-    dump(v_set2.labels_particle_IDS, open(saving_path + 'larger_labels_validation_set.pkl', 'wb'))
+    # saving_path = path_uniform + "2.5k_in_each_80bins/"
+    # n_samples = 2500
+    #
+    # training_set = tn.InputsPreparation(train_sims, shuffle=True, scaler_type="minmax", return_rescaled_outputs=True,
+    #                                     output_range=(-1, 1), load_ids=False, random_subset_each_sim=None,
+    #                                     log_high_mass_limit=13.4,
+    #                                     random_style="uniform", num_per_mass_bin=n_samples, num_bins=80,
+    #                                     path=path_sims)
+    #
+    # dump(training_set.particle_IDs, open(saving_path + 'training_set.pkl', 'wb'))
+    # dump(training_set.labels_particle_IDS, open(saving_path + 'labels_training_set.pkl', 'wb'))
+    # dump(training_set.scaler_output, open(saving_path + 'scaler_output.pkl', 'wb'))
+    #
+    # v_set = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, shuffle=True,
+    #                              random_style="random", random_subset_all=5000, random_subset_each_sim=1000000,
+    #                              log_high_mass_limit=13.4,
+    #                              scaler_output=training_set.scaler_output, path=path_sims)
+    # dump(v_set.particle_IDs, open(saving_path + 'validation_set.pkl', 'wb'))
+    # dump(v_set.labels_particle_IDS, open(saving_path + 'labels_validation_set.pkl', 'wb'))
+    #
+    # v_set2 = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False,
+    #                               log_high_mass_limit=13.4, random_style="random", random_subset_all=50000,
+    #                               random_subset_each_sim=1000000,
+    #                               scaler_output=training_set.scaler_output, path=path_sims)
+    # dump(v_set2.particle_IDs, open(saving_path + 'larger_validation_set.pkl', 'wb'))
+    # dump(v_set2.labels_particle_IDS, open(saving_path + 'larger_labels_validation_set.pkl', 'wb'))
 
 
