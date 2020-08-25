188a1d7567da8070039ee741160aa4f7761256dc
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-08-25 17:24:16 +0200

    5e-5 lr

--- scripts/regression/mass_range_13.4/train_20sims_random_200k_alpha-2.5_lr5e-5.py
+++ scripts/regression/mass_range_13.4/train_20sims_random_200k_alpha-2.5_lr5e-5.py
@@ -78,6 +78,6 @@ if __name__ == "__main__":
                           dim=generator_training.dim, initialiser="Xavier_uniform", max_queue_size=8,
                           use_multiprocessing=False, workers=0, verbose=1, num_gpu=1, lr=lr, save_summary=True,
                           path_summary=saving_path, validation_freq=1, train=True, compile=True,
-                          initial_epoch=None,
+                          initial_epoch=None, lr_scheduler=False,
                           seed=seed)
 
