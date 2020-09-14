e3f2bf276c3269a4cae2fd428279243b17db1bbd
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-09-14 14:43:08 +0200

    averaged density boxes

--- scripts/regression/mass_range_13.4/train_averaged_density_boxes.py
+++ scripts/regression/mass_range_13.4/train_averaged_density_boxes.py
@@ -11,10 +11,10 @@ import os
 
 if __name__ == "__main__":
 
-########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
+    ########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
 
     saving_path = "/mnt/beegfs/work/ati/pearl037/regression/mass_range_13.4/random_20sims_200K/averaged_densities" \
-                  "/log_alpha_-2.4/"
+                  "/no_reg/"
     seed = 123
 
     # Load data
@@ -45,9 +45,9 @@ if __name__ == "__main__":
 
     ######### TRAIN THE MODEL ################
 
-    alpha = 10**-2.4
+    # alpha = 10**-3.5
     params_all_conv = {'activation': "linear", 'relu': True, 'strides': 1, 'padding': 'same', 'bn': False,
-                       'kernel_regularizer': reg.l2_norm(alpha)
+                       # 'kernel_regularizer': reg.l2_norm(alpha)
                        }
     param_conv = {'conv_1': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
                   'conv_2': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
@@ -61,18 +61,18 @@ if __name__ == "__main__":
     # Dense layers parameters
 
     params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True,
-                      'kernel_regularizer': reg.l1_and_l21_group(alpha)}
+                      # 'kernel_regularizer': reg.l1_and_l21_group(alpha)
+                      }
     param_fcc = {'dense_1': {'neurons': 256, **params_all_fcc}, 'dense_2': {'neurons': 128, **params_all_fcc},
                  'last': {}}
 
     lr = 0.00005
     Model = CNN.CNNCauchy(param_conv, param_fcc, model_type="regression", training_generator=generator_training,
-                          shuffle=True, validation_generator=generator_validation, num_epochs=20,
-                          metrics=[CNN.likelihood_metric],
+                          shuffle=True, validation_generator=generator_validation, num_epochs=30,
+                          # metrics=[CNN.likelihood_metric],
                           steps_per_epoch=len(generator_training), validation_steps=len(generator_validation),
                           dim=generator_training.dim, initialiser="Xavier_uniform", max_queue_size=8,
                           use_multiprocessing=False, workers=0, verbose=1, num_gpu=1, lr=lr, save_summary=True,
                           path_summary=saving_path, validation_freq=1, train=True, compile=True,
                           initial_epoch=None, lr_scheduler=False,
                           seed=seed)
-
