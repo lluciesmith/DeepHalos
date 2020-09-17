c864d0eacdefccc4763040bfab05ea16afd511d6
Luisa Lucie-Smith <ucapllu@ucl.ac.uk>
2020-09-17 12:21:02 +0200

    evaluate loss

--- scripts/regression/averaged_densities/evaluate_loss.py
+++ scripts/regression/averaged_densities/evaluate_loss.py
@@ -2,6 +2,7 @@ from dlhalos_code import CNN
 import dlhalos_code.data_processing as tn
 from pickle import load
 import numpy as np
+from dlhalos_code import custom_regularizers as reg
 
 
 if __name__ == "__main__":
@@ -9,7 +10,7 @@ if __name__ == "__main__":
     ########### CREATE GENERATORS FOR TRAINING AND VALIDATION #########
 
     saving_path = "/mnt/beegfs/work/ati/pearl037/regression/mass_range_13.4/random_20sims_200K/averaged_densities" \
-                   "/no_reg/"
+                   "/log_alpha_-4/"
 
     path_sims = "/mnt/beegfs/work/ati/pearl037/"
     all_sims = ["%i" % i for i in np.arange(22)]
@@ -17,28 +18,36 @@ if __name__ == "__main__":
     s = tn.SimulationPreparation(all_sims, path=path_sims)
 
     path_data = "/mnt/beegfs/work/ati/pearl037/regression/training_set_13.4/20sims/random/200k/"
-    training_particle_IDs = load(open(path_data + 'training_set.pkl', 'rb'))
-    training_labels_particle_IDS = load(open(path_data + 'labels_training_set.pkl', 'rb'))
-    val_particle_IDs = load(open(path_data + 'validation_set.pkl', 'rb'))
-    val_labels_particle_IDS = load(open(path_data + 'labels_validation_set.pkl', 'rb'))
+    # training_particle_IDs = load(open(path_data + 'training_set.pkl', 'rb'))
+    # training_labels_particle_IDS = load(open(path_data + 'labels_training_set.pkl', 'rb'))
+    val_particle_IDs = load(open(path_data + 'larger_validation_set.pkl', 'rb'))
+    val_labels_particle_IDS = load(open(path_data + 'larger_labels_validation_set.pkl', 'rb'))
+
+    # tr_labels = np.array([training_labels_particle_IDS[ID] for ID in training_particle_IDs])
+    # tr_particle_below_12 = list(np.array(training_particle_IDs)[tr_labels <= 0])
+    #
+    # val_labels = np.array([val_labels_particle_IDS[ID] for ID in val_particle_IDs])
+    # val_particle_below_12 = list(np.array(val_particle_IDs)[val_labels <= 0])
 
     # Create the generators for training
 
     dim = (75, 75, 75)
     params_box = {'input_type': 'averaged', 'num_shells': 20}
 
-    params_tr = {'batch_size': 64, 'rescale_mean': 1.005, 'rescale_std': 0.05050, 'dim': dim}
-    generator_training = tn.DataGenerator(training_particle_IDs, training_labels_particle_IDS, s.sims_dic,
-                                          shuffle=False, **params_tr, **params_box)
+    # params_tr = {'batch_size': 64, 'rescale_mean': 1.005, 'rescale_std': 0.05050, 'dim': dim}
+    # generator_training = tn.DataGenerator(training_particle_IDs, training_labels_particle_IDS, s.sims_dic,
+    #                                   shuffle=False, **params_tr, **params_box)
     params_val = {'batch_size': 50, 'rescale_mean': 1.005, 'rescale_std': 0.05050, 'dim': dim}
     generator_validation = tn.DataGenerator(val_particle_IDs, val_labels_particle_IDS, s.sims_dic,
-                                            shuffle=False, **params_val, **params_box)
+                                        shuffle=False, **params_val, **params_box)
 
 
     ######### TRAIN THE MODEL ################
 
+    alpha = 10**-4
+
     params_all_conv = {'activation': "linear", 'relu': True, 'strides': 1, 'padding': 'same', 'bn': False,
-                       #'kernel_regularizer': reg.l2_norm(alpha)
+                       'kernel_regularizer': reg.l2_norm(alpha)
                        }
     param_conv = {'conv_1': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
                   'conv_2': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
@@ -51,7 +60,8 @@ if __name__ == "__main__":
 
     # Dense layers parameters
 
-    params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True}
+    params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True,
+                      'kernel_regularizer': reg.l1_and_l21_group(alpha)}
     param_fcc = {'dense_1': {'neurons': 256, **params_all_fcc}, 'dense_2': {'neurons': 128, **params_all_fcc},
                  'last': {}}
 
@@ -62,21 +72,19 @@ if __name__ == "__main__":
                           save_summary=False, path_summary=saving_path, validation_freq=1, train=False, compile=True,
                           initial_epoch=None, initialiser="Xavier_uniform")
 
-    epochs = ["%02d" % num for num in np.arange(1, 17)]
+    epochs = ["%02d" % num for num in np.arange(1, 39)][::5]
     loss_val = []
-    loss_tr = []
+    # loss_tr = []
 
     for num_epoch in epochs:
-
         weights = saving_path + "model/weights." + num_epoch + ".h5"
         Model.model.load_weights(weights)
-
-        l_tr = Model.model.evaluate_generator(generator_training, use_multiprocessing=False, workers=0, verbose=1,
-                                              steps=100)
-        loss_tr.append(l_tr)
-
-        l_v = Model.model.evaluate_generator(generator_validation, use_multiprocessing=False, workers=0, verbose=1)
+        # l_tr = Model.model.evaluate_generator(generator_training, use_multiprocessing=False, workers=0, verbose=1,
+        #                                       steps=len(generator_training))
+        # loss_tr.append(l_tr)
+        l_v = Model.model.evaluate_generator(generator_validation, use_multiprocessing=False, workers=0, verbose=1,
+                                             steps=len(generator_validation))
         loss_val.append(l_v)
 
-    np.save(saving_path + "loss_training_end_epoch.npy", loss_tr)
-    np.save(saving_path + "loss_val_end_epoch.npy", loss_val)
+    # np.save(saving_path + "loss_training_end_epoch_below_12.npy", loss_tr)
+    np.save(saving_path + "loss_larger_validation_set.npy", loss_val)
