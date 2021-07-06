8a0bc84627f03148ba722de0e997c2049f8b2e74
Luisa Lucie-Smith <luisals@macbookpro.local>
2021-07-06 13:02:40 +0200

    updated potential inputs module

--- dlhalos_code/potential.py
+++ dlhalos_code/potential.py
@@ -7,33 +7,33 @@ def get_delta_in_fourier_space(delta):
     return delta_fourier
 
 
-def get_k_coordinates_box(boxsize):
+def get_k_coordinates_box(boxsize, path='/mnt/beegfs/home/pearl037/'):
     """ Return the k-coordinates of the simulation box """
-    a = np.load('/mnt/beegfs/home/pearl037/Fourier_transform_matrix.npy')
+    a = np.load(path + 'Fourier_transform_matrix.npy')
     k_coord = 2. * np.pi * a / boxsize
     return k_coord
 
 
-def get_norm_k_box(boxsize):
-    k_coord = get_k_coordinates_box(boxsize)
+def get_norm_k_box(boxsize, path='/mnt/beegfs/home/pearl037/'):
+    k_coord = get_k_coordinates_box(boxsize, path=path)
     norm_squared = np.power(np.fabs(k_coord), 2)
     return norm_squared
 
 
-def poisson_equation_in_fourier_space(delta_fourier, boxsize):
+def poisson_equation_in_fourier_space(delta_fourier, boxsize, path='/mnt/beegfs/home/pearl037/'):
     """ Poisson's equation in Fourier space """
-    norm_k_squared = get_norm_k_box(boxsize)
+    norm_k_squared = get_norm_k_box(boxsize, path=path)
     norm_k_squared[0, 0, 0] = 1
     phi = - delta_fourier / norm_k_squared
     phi[0, 0, 0] = 0
     return phi
 
 
-def get_potential_from_density(delta, boxsize):
+def get_potential_from_density(delta, boxsize, path='/mnt/beegfs/home/pearl037/'):
     """
     Computes the gravitational potential give the density via Poisson's equation in Fourier space.
     """
     delta_fourier = get_delta_in_fourier_space(delta)
-    potential_fourier = poisson_equation_in_fourier_space(delta_fourier, boxsize)
+    potential_fourier = poisson_equation_in_fourier_space(delta_fourier, boxsize, path=path)
     potential_real = np.real(np.fft.ifftn(potential_fourier)).flatten()
     return potential_real
