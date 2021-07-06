0cf9f543acf9917629b74ee17503e1d947e12bd3
Luisa Lucie-Smith <luisals@macbookpro.local>
2021-07-06 16:28:38 +0200

    train on averaged density in the large-box

---
+++ scripts/bigbox/params_bigbox_avg.py
@@ -0,0 +1,58 @@
+from pickle import load
+import numpy as np
+from dlhalos_code import custom_regularizers as reg
+
+seed = 123
+log_alpha = -2.5
+alpha = 10**float(log_alpha)
+
+saving_path = "/mnt/beegfs/work/ati/pearl037/regression/bigbox/averaged/alpha_" + str(log_alpha) + "/"
+
+# Data parameters
+
+path_sims = "/mnt/beegfs/work/ati/pearl037/"
+all_sims = ["L200_N1024_genetIC", "L200_N1024_genetIC2", "L200_N1024_genetIC3"]
+
+path_data = "/mnt/beegfs/work/ati/pearl037/regression/bigbox/data/"
+training_particle_IDs = load(open(path_data + 'training_set.pkl', 'rb'))
+training_labels_particle_IDS = load(open(path_data + 'labels_training_set.pkl', 'rb'))
+val_particle_IDs = load(open(path_data + 'validation_set.pkl', 'rb'))
+val_labels_particle_IDS = load(open(path_data + 'labels_validation_set.pkl', 'rb'))
+
+dim = (75, 75, 75)
+params_box = {'input_type': 'averaged', 'num_shells': 20}
+params_tr = {'batch_size': 64, 'rescale_mean': 0., 'rescale_std': 65, 'dim': dim}
+params_val = {'batch_size': 50, 'rescale_mean': 0., 'rescale_std': 65, 'dim': dim}
+
+
+# Model parameters
+
+lr = 0.00005
+
+params_all_conv = {'activation': "linear", 'relu': True, 'strides': 1, 'padding': 'same', 'bn': False,
+                   'kernel_regularizer': reg.l2_norm(alpha)
+                   }
+param_conv = {'conv_1': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': None, **params_all_conv},
+              'conv_2': {'num_kernels': 32, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+              'conv_3': {'num_kernels': 64, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+              'conv_4': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+              'conv_5': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv},
+              'conv_6': {'num_kernels': 128, 'dim_kernel': (3, 3, 3), 'pool': "max", **params_all_conv}
+              }
+
+# Dense layers parameters
+
+params_all_fcc = {'bn': False, 'activation': "linear", 'relu': True,
+                  'kernel_regularizer': reg.l1_and_l21_group(alpha)}
+param_fcc = {'dense_1': {'neurons': 256, **params_all_fcc}, 'dense_2': {'neurons': 128, **params_all_fcc},
+             'last': {}}
+
+
+# Parameters for testing
+training_set_testing = False
+val_sim = all_sims[:-1]
+# num_epoch_testing = "09"
+#
+scaler = load(open(path_data + 'scaler_output.pkl', 'rb'))
+larger_val_particle_IDs = load(open(path_data + 'larger_validation_set.pkl', 'rb'))
+larger_val_labels_particle_IDS = load(open(path_data + 'larger_labels_validation_set.pkl', 'rb'))
---
+++ scripts/bigbox/training_avg.py
@@ -0,0 +1,35 @@
+from dlhalos_code import CNN
+import dlhalos_code.data_processing as tn
+import numpy as np
+import tensorflow as tf
+import random as python_random
+import sys
+import importlib
+
+
+if __name__ == "__main__":
+    try: params = importlib.import_module(sys.argv[1])
+    except IndexError: from scripts.bigbox import params_bigbox_avg as params
+
+    np.random.seed(params.seed)
+    python_random.seed(params.seed)
+    tf.compat.v1.set_random_seed(params.seed)
+
+    # Prepare the data
+
+    s = tn.SimulationPreparation(params.all_sims, path=params.path_sims)
+    generator_training = tn.DataGenerator(params.training_particle_IDs, params.training_labels_particle_IDS, s.sims_dic,
+                                          shuffle=True, **params.params_tr, **params.params_box)
+    generator_validation = tn.DataGenerator(params.val_particle_IDs, params.val_labels_particle_IDS, s.sims_dic,
+                                            shuffle=False, **params.params_val, **params.params_box)
+
+
+    ######### TRAIN THE MODEL ################
+    Model = CNN.CNNCauchy(params.param_conv, params.param_fcc, model_type="regression",
+                          training_generator=generator_training, steps_per_epoch=len(generator_training),
+                          validation_generator=generator_validation, validation_steps=len(generator_validation),
+                          num_epochs=40, shuffle=True, metrics=[CNN.likelihood_metric], dim=generator_training.dim,
+                          initialiser="Xavier_uniform", max_queue_size=10,
+                          use_multiprocessing=False, workers=0, verbose=1, num_gpu=1, lr=params.lr, save_summary=True,
+                          path_summary=params.saving_path, validation_freq=1, train=True, compile=True,
+                          lr_scheduler=False, seed=params.seed)
