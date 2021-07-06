8c502679b90477d7ff073416454de5c46cd5130c
Luisa Lucie-Smith <luisals@macbookpro.local>
2021-07-06 13:17:14 +0200

    paths

--- scripts/bigbox/data.py
+++ scripts/bigbox/data.py
@@ -5,9 +5,10 @@ import numpy as np
 if __name__ == "__main__":
     #path = "/mnt/beegfs/work/ati/pearl037/regression/bigbox/data/"
     path = "/freya/ptmp/mpa/luisals/Simulations/deep_halos/"
+    saving_path = path + "data/"
 
     #path_sims = "/mnt/beegfs/work/ati/pearl037/"
-    path_sims = "/freya/ptmp/mpa/luisals/Simulations/"
+    # path_sims = "/freya/ptmp/mpa/luisals/Simulations/"
     all_sims = ["L200_N1024_genetIC", "L200_N1024_genetIC2", "L200_N1024_genetIC3"]
     train_sims = all_sims[:-1]
     val_sim = all_sims[-1]
@@ -17,7 +18,7 @@ if __name__ == "__main__":
                                         output_range=(-1, 1), log_high_mass_limit=13.4,
                                         load_ids=False, random_subset_each_sim=None,
                                         random_style="random", random_subset_all=n_samples,
-                                        path=path_sims)
+                                        path=path)
 
     dump(training_set.particle_IDs, open(saving_path + 'training_set.pkl', 'wb'))
     dump(training_set.labels_particle_IDS, open(saving_path + 'labels_training_set.pkl', 'wb'))
@@ -26,12 +27,12 @@ if __name__ == "__main__":
     v_set = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, shuffle=True,
                                  random_style="random", log_high_mass_limit=13.4,
                                  random_subset_all=5000, random_subset_each_sim=1000000,
-                                 scaler_output=training_set.scaler_output, path=path_sims)
+                                 scaler_output=training_set.scaler_output, path=path)
     dump(v_set.particle_IDs, open(saving_path + 'validation_set.pkl', 'wb'))
     dump(v_set.labels_particle_IDS, open(saving_path + 'labels_validation_set.pkl', 'wb'))
 
     v_set2 = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, log_high_mass_limit=13.4,
                                  random_style="random", random_subset_all=50000, random_subset_each_sim=1000000,
-                                 scaler_output=training_set.scaler_output, path=path_sims)
+                                 scaler_output=training_set.scaler_output, path=path)
     dump(v_set2.particle_IDs, open(saving_path + 'larger_validation_set.pkl', 'wb'))
     dump(v_set2.labels_particle_IDS, open(saving_path + 'larger_labels_validation_set.pkl', 'wb'))
