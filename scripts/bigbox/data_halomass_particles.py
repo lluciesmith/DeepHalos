b5c878f4abeb75d394c71885369472b9601f1760
Luisa Lucie-Smith <luisals@macbookpro.local>
2021-07-06 11:35:13 +0200

    get halo mass label for particles in new L=200, N=1024^3 boxes

--- scripts/bigbox/data_halomass_particles.py
+++ scripts/bigbox/data_halomass_particles.py
@@ -6,15 +6,15 @@ if __name__ == "__main__":
     sims = ["L200_N1024_genetIC", "L200_N1024_genetIC2", "L200_N1024_genetIC3"]
 
     for sim in sims:
-        saving_path = "/mnt/beegfs/work/ati/pearl037/" + sim
+        saving_path = "/freya/ptmp/mpa/luisals/deep_halos/"
 
-        fp = pynbody.load(saving_path + "/snapshots/snapshot_000.hdf5")
+        fp = pynbody.load("/freya/ptmp/mpa/luisals/Simulations/" + sim + "/output/snapshot_000.hdf5")
         orig_unit = pynbody.units.g / pynbody.units.h * float(fp.infer_original_units('kg'))
         units_ratio = pynbody.units.Msol.ratio(orig_unit, h=fp.properties['h'])
         ids = fp['iord']
 
-        h = h5py.File(saving_path + "/snapshots/fof_subhalo_tab_000.hdf5", "r")
-        offset = h['Group']['GroupOffsetType'][:,1]
+        h = h5py.File("/freya/ptmp/mpa/luisals/Simulations/" + sim + "/output/fof_subhalo_tab_000.hdf5", "r")
+        offset = h['Group']['GroupOffsetType'][:, 1]
         length = h['Group']['GroupLen'][:]
 
         m = h['Group']['GroupMass'][:]
@@ -23,5 +23,5 @@ if __name__ == "__main__":
             ind = ids[offset[i]: offset[i] + length[i]]
             halo_mass_ids[ind] = m[i] / units_ratio
 
-        np.save(saving_path + "/reseed" + sim + "_halo_mass_particles.npy", halo_mass_ids)
+        np.save(saving_path + sim + "_halo_mass_particles.npy", halo_mass_ids)
         del fp, h, halo_mass_ids
