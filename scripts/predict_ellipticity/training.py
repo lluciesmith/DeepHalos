17c6acb790eb6d9c557f62171952b7d2eb93b69a
Luisa Lucie-Smith <luisals@macbookpro.local>
2022-03-18 14:52:30 +0100

    import statement

--- scripts/predict_ellipticity/training.py
+++ scripts/predict_ellipticity/training.py
@@ -7,6 +7,7 @@ import sys
 import importlib
 from pickle import dump
 from collections import OrderedDict
+import sklearn.preprocessing
 
 
 def turn_mass_labels_into_ellipticity(labels, scale, path, scaler=None):
