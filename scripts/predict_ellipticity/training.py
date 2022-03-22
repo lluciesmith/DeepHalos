379d0f2e767672235e67c431a65bdb8d14f7804b
Luisa Lucie-Smith <luisals@macbookpro.local>
2022-03-22 08:27:18 +0100

    mse metric

--- scripts/predict_ellipticity/training.py
+++ scripts/predict_ellipticity/training.py
@@ -62,9 +62,8 @@ if __name__ == "__main__":
     ######### TRAIN THE MODEL ################
 
     Model = CNN.CNN(params.param_conv, params.param_fcc, model_type="regression", training_generator=generator_training,
-                          shuffle=True, validation_generator=generator_validation, num_epochs=30,
-                          metrics=[CNN.likelihood_metric],
-                          steps_per_epoch=len(generator_training), validation_steps=len(generator_validation),
+                    shuffle=True, validation_generator=generator_validation, num_epochs=30, metrics=['mse'],
+                    steps_per_epoch=len(generator_training), validation_steps=len(generator_validation),
                           dim=generator_training.dim, initialiser="Xavier_uniform", max_queue_size=10,
                           use_multiprocessing=False, workers=0, verbose=1, num_gpu=1, lr=params.lr, save_summary=True,
                           path_summary=params.saving_path, validation_freq=1, train=True, compile=True,
