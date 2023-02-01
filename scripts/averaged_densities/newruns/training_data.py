5b858b8620f3d0f8e43bb87a9509874e448d9785
Luisa Lucie-Smith <luisals@mpa-garching.mpg.de>
2023-02-01 14:01:11 +0100

    get training data

--- scripts/averaged_densities/newruns/training_data.py
+++ scripts/averaged_densities/newruns/training_data.py
@@ -11,9 +11,8 @@ if __name__ == "__main__":
     test_sim = ["6", "22", "23", "24"]
     train_sims = list(np.array(all_sims)[np.where(~np.in1d(all_sims, test_sim) & ~np.in1d(all_sims, val_sim))[0]])
 
-    # Save training sets sampled at random from 9 simulations, with n=200,000
-
-    n_samples = 100
+    # training set
+    n_samples = 200000
     saving_path = "/share/hypatia/lls/newdlhalos/training_data/"
 
     training_set = tn.InputsPreparation(train_sims, shuffle=True, scaler_type="minmax", return_rescaled_outputs=True,
@@ -25,20 +24,21 @@ if __name__ == "__main__":
     dump(training_set.labels_particle_IDS, open(saving_path + 'labels_training_set.pkl', 'wb'))
     dump(training_set.scaler_output, open(saving_path + 'scaler_output.pkl', 'wb'))
 
-    v_set = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, shuffle=True,
+    # validation set
+    v_set = tn.InputsPreparation(val_sim, scaler_type="minmax", load_ids=False, shuffle=True,
                                  random_style="random", log_high_mass_limit=13.4,
-                                 random_subset_all=5000, random_subset_each_sim=1000000,
+                                 random_subset_all=5000, random_subset_each_sim=None,
                                  scaler_output=training_set.scaler_output, path=path_sims)
     dump(v_set.particle_IDs, open(saving_path + 'validation_set.pkl', 'wb'))
     dump(v_set.labels_particle_IDS, open(saving_path + 'labels_validation_set.pkl', 'wb'))
 
-    test_set = tn.InputsPreparation([val_sim], scaler_type="minmax", load_ids=False, log_high_mass_limit=13.4,
-                                    random_style="random", random_subset_all=50000, random_subset_each_sim=1000000,
+    # test set
+    test_set = tn.InputsPreparation(test_sim, scaler_type="minmax", load_ids=False, log_high_mass_limit=13.4,
+                                    random_style="random", random_subset_all=50000, random_subset_each_sim=None,
                                     scaler_output=training_set.scaler_output, path=path_sims)
     # see if there are test set particles from sim 6 that are also in the validation set
     idx = ~np.in1d(test_set.particle_IDs, v_set.particle_IDs)
     test_setIDs = list(np.array(test_set.particle_IDs)[np.where(idx)[0]])
     labels_test_setIDs = {key: test_set.labels_particle_IDS[key] for key in test_setIDs}
-
     dump(test_setIDs, open(saving_path + 'test_set.pkl', 'wb'))
     dump(labels_test_setIDs, open(saving_path + 'labels_test_set.pkl', 'wb'))
