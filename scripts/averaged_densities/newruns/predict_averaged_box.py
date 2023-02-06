a076e97319239cc1239c3cc486ae36b323ab4bb2
Luisa Lucie-Smith <luisals@mpa-garching.mpg.de>
2023-02-06 10:23:02 +0100

    predictions

--- scripts/averaged_densities/newruns/predict_averaged_box.py
+++ scripts/averaged_densities/newruns/predict_averaged_box.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     generator_validation = tn.DataGenerator(params.val_particle_IDs, params.val_labels_particle_IDS, s.sims_dic,
                                             shuffle=False, path=params.path_data, **params.params_val, **params.params_box)
     generator_test = tn.DataGenerator(params.test_particle_IDs, params.test_labels_particle_IDS, s.sims_dic,
-                                      shuffle=False, **params.params_val, **params.params_box)
+                                      shuffle=False, path=params.path_data, **params.params_val, **params.params_box)
 
     ######### Load THE MODEL ################
 
@@ -31,6 +31,13 @@ if __name__ == "__main__":
     Model.model.load_weights(weights)
 
     # Predict
+    pred = Model.model.predict_generator(generator_validation, use_multiprocessing=False, workers=0, verbose=1)
+    truth_rescaled = np.array([params.val_labels_particle_IDS[ID] for ID in params.val_particle_IDs])
+    h_m_pred = params.scaler.inverse_transform(pred.reshape(-1, 1)).flatten()
+    true = params.scaler.inverse_transform(truth_rescaled.reshape(-1, 1)).flatten()
+    np.save(params.saving_path + "predicted_sim_valset_epoch_" + num_epoch_testing + ".npy", h_m_pred)
+    np.save(params.saving_path + "true_sim_valset_epoch_" + num_epoch_testing + ".npy", true)
+
     pred = Model.model.predict_generator(generator_test, use_multiprocessing=False, workers=0, verbose=1)
     truth_rescaled = np.array([params.test_labels_particle_IDS[ID] for ID in params.test_particle_IDs])
     h_m_pred = params.scaler.inverse_transform(pred.reshape(-1, 1)).flatten()
