87066a1192758ba58693d5a7fc1c2994da321041
Luisa Lucie-Smith <luisals@mpa-garching.mpg.de>
2023-02-09 20:12:12 +0100

    change cache path

--- scripts/averaged_densities/newruns/hybrid_inputs/training_hybrid_box.py
+++ scripts/averaged_densities/newruns/hybrid_inputs/training_hybrid_box.py
@@ -9,13 +9,15 @@ if __name__ == "__main__":
     params = importlib.import_module(sys.argv[1])
     print(params.log_alpha)
 
+    cache_path = '/share/data2/lls/'
+
     # Create the generators for training
     s = tn.SimulationPreparation(params.all_sims, path=params.path_sims)
     generator_training = tn.DataGenerator(params.training_particle_IDs, params.training_labels_particle_IDS, s.sims_dic,
-                                          shuffle=True, path=params.path_data, cache_path=params.path_data + "hybrid_tset",
+                                          shuffle=True, path=params.path_data, cache_path=cache_path + "hybrid_tset",
                                           **params.params_tr, **params.params_box)
     generator_validation = tn.DataGenerator(params.val_particle_IDs, params.val_labels_particle_IDS, s.sims_dic,
-                                            shuffle=False, path=params.path_data, cache_path=params.path_data + "hybrid_vset",
+                                            shuffle=False, path=params.path_data, cache_path=cache_path + "hybrid_vset",
                                             **params.params_val, **params.params_box)
     tset = generator_training.get_dataset()
     vset = generator_validation.get_dataset()
